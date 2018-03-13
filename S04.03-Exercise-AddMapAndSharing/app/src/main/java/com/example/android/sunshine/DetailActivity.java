b7f32d4a1569b963d075d7318365a3e457056a58
Samone <samonemorris@gmail.com>
2018-03-13 13:36:21 -0500

    Sunshine Exercise S04.03 : Completed the Override methods onOptionsItemSelected() and onOptionsMenuCreated() methods for the Share functionality in DetailActivity

--- S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ S04.03-Exercise-AddMapAndSharing/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,8 +1,18 @@
+/* -------------------------------------------------------------------------------------------------
+ * Activity : Detail Activity
+ *
+ * @author Samone Morris
+ * @date   03/13/2018
+ * -------------------------------------------------------------------------------------------------
+ */
 package com.example.android.sunshine;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -29,6 +39,32 @@ public class DetailActivity extends AppCompatActivity {
         }
     }
 
-    // TODO (3) Create a menu with an item with id of action_share
-    // TODO (4) Display the menu and implement the forecast sharing functionality
+    // COMPLETED (3) Create a menu with an item with id of action_share
+    // COMPLETED (4) Display the menu and implement the forecast sharing functionality
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate( R.menu.share, menu );
+
+        return true;
+    }// end onCreateOptionsMenu(...)
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if( id == R.id.action_share ){
+            ShareCompat.IntentBuilder
+                       .from( this )
+                       .setType( "text/plain" )
+                       .setChooserTitle( FORECAST_SHARE_HASHTAG )
+                       .setText( mForecast )
+                       .startChooser();
+
+            return true;
+        }// end if
+
+        return super.onOptionsItemSelected(item);
+    }// end onOptionsItemSelected(...)
+
 }
