dd8a29a9a718fd20469ddece500ad4763f175c47
Samone <samonemorris@gmail.com>
2018-04-11 18:42:00 -0500

    Sunshine Exercise S09.03 : Completed the delete() method in the WeatherProvider class.

--- S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
+++ S09.03-Exercise-ContentProviderDelete/app/src/main/java/com/example/android/sunshine/data/WeatherProvider.java
@@ -12,12 +12,17 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * @author Samone Morris
+ * @date   04/11/18
  */
 package com.example.android.sunshine.data;
 
 import android.annotation.TargetApi;
 import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -293,7 +298,7 @@ public class WeatherProvider extends ContentProvider {
         return cursor;
     }
 
-//  TODO (1) Implement the delete method of the ContentProvider
+//  COMPLETED (1) Implement the delete method of the ContentProvider
     /**
      * Deletes data at a given URI with optional arguments for more fine tuned deletions.
      *
@@ -304,12 +309,38 @@ public class WeatherProvider extends ContentProvider {
      */
     @Override
     public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-        throw new RuntimeException("Student, you need to implement the delete method!");
+        int rowsDeleted;
+        final SQLiteDatabase db = mOpenHelper.getWritableDatabase();
 
-//          TODO (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
+        int match_code = sUriMatcher.match( uri );
 
-//      TODO (3) Return the number of rows deleted
-    }
+        switch ( match_code ){
+            case CODE_WEATHER:
+                //  COMPLETED (2) Only implement the functionality, given the proper URI, to delete ALL rows in the weather table
+
+                // Make sure we have at most 1 item to delete
+                if( selection == null ){ selection = "1"; }
+
+                rowsDeleted = db.delete(
+                        WeatherContract.WeatherEntry.TABLE_NAME,
+                        selection,
+                        selectionArgs
+                );
+                break;
+
+                default:
+                    throw new UnsupportedOperationException( "Cannot perform delete operation on URI : " + uri)
+        }// end switch
+
+//      COMPLETED (3) Return the number of rows deleted
+        if( rowsDeleted > 0 ){
+            Context context = getContext();
+            ContentResolver resolver = context.getContentResolver();
+            resolver.notifyChange( uri, null );
+        }// end if
+
+        return rowsDeleted;
+    }// end delete(...)
 
     /**
      * In Sunshine, we aren't going to do anything with this method. However, we are required to
